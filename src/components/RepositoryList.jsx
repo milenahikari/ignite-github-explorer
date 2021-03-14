6f9f19e302b1366761c102c69bccfcf811aba482
Milena <milenahikari11@gmail.com>
2021-03-14 20:05:19 -0300

    Criando componente repository item

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,17 +1,19 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform'
+};
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
